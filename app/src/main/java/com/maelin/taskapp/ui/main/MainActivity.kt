30d42aa1d070c57a989207f620334d2358658d39
Julia Miklosovicova <miklosovicl@gmail.com>
2023-10-20 15:01:24 +0200

    fix problem where task list pushed button out of screen when it was filled with items

--- app/src/main/java/com/maelin/taskapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/maelin/taskapp/ui/main/MainActivity.kt
@@ -3,24 +3,33 @@ package com.maelin.taskapp.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,19 +37,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.maelin.taskapp.ui.theme.TaskAppTheme
 import com.maelin.taskapp.ui.theme.Typography
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val mainViewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +52,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column {
                         Greeting()
-                        Spacer(modifier = Modifier.weight(1f))
+                        Column (modifier = Modifier.weight(1f)) {
+                            IntegerList()
+                        }
                         IncreaseButton()
                     }
 
@@ -77,15 +78,21 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    val viewModel = hiltViewModel<MainViewModel>()a
     TaskAppTheme {
-        Column {
-            Greeting()
-            IntegerList()
-            Spacer(modifier = Modifier.weight(1f))
-            IncreaseButton()
-        }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Greeting()
+                Column (modifier = Modifier.weight(1f).
+                                    then(Modifier.background(Color.Red))) {
+                    IntegerList()
+                }
 
+                //Spacer(modifier = Modifier.weight(1f))
+                IncreaseButton()
+            }
     }
 }
 
@@ -102,10 +109,7 @@ fun IncreaseButton() {
 @Composable
 fun IntegerList() {
     val viewModel = hiltViewModel<MainViewModel>()
-    LazyColumn (
-        modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(16.dp)
-    ) {
+    LazyColumn (contentPadding = PaddingValues(16.dp)) {
         items(viewModel.getItems()) {
             item -> ListRow(content = item)
         }
